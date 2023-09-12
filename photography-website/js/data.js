795ab4b7301591174aff243099bf019f56a7106c
SAS0109 <144659572+SAS0109@users.noreply.github.com>
2023-09-12 19:13:43 +0530

    Add files via upload

---
+++ photography-website/js/data.js
@@ -0,0 +1,102 @@
+mainData = [
+    { 
+      id: 2,
+      type: "Astrophotography",
+      src: 'img/gallery2.jpg',
+      tum: 'img/gallery/2.jpg',
+      cp: "Man must rise above the Earth—to the top of the atmosphere and beyond—for only thus will he fully understand the world in which he lives."
+    },
+    { 
+      id: 3,
+      type: "Astrophotography",
+      src: 'img/gallery/3.jpg',
+      tum: 'img/gallery/3.jpg',
+      cp: "Astronomy compels the soul to look upward, and leads us from this world to another"
+    },
+    { 
+      id: 4,
+      type: "Nature",
+      src: 'img/gallery/4.jpg',
+      tum: 'img/gallery/4.jpg',
+      cp: "Somewhere,something incredible is waiting to be known"
+    },
+    { 
+      id: 5,
+      type: "Nature",
+      src: 'img/gallery/5.jpg',
+      tum: 'img/gallery/5.jpg',
+      cp: "The history of astronomy is the history of receding horizons"
+    },
+    { 
+      id: 6,
+      type: "Nature",
+      src: 'img/gallery/6.jpg',
+      tum: 'img/gallery/6.jpg',
+      cp: "The universe is a big place,perhaps the biggest"
+    },
+    { 
+      id: 7,
+      type: "Nature",
+      src: 'img/gallery/7.jpg',
+      tum: 'img/gallery/7.jpg',
+      cp: "So much astronomy and so less time"
+    },
+    { 
+      id: 8,
+      type: "Astrophotography",
+      src: 'img/gallery/8.jpg',
+      tum: 'img/gallery/8.jpg',
+      cp: "Darkness exists to make light truly count"
+    },
+    { 
+      id: 9,
+      type: "Astrophotography",
+      src: 'img/gallery/9.jpg',
+      tum: 'img/gallery/9.jpg',
+      cp: "We all the stars in this galaxy.All of us.No one's greater than other."
+    },
+    { 
+      id: 10,
+      type: "Astrophotography",
+      src: 'img/gallery/10.jpg',
+      tum: 'img/gallery/10.jpg',
+      cp: "Stars can't work without the darkness"
+    },
+    { 
+      id: 11,
+      type: "Astrophotography",
+      src: 'img/gallery/11.jpg',
+      tum: 'img/gallery/11.jpg',
+      cp: "There are far more galaxies than people"
+    },{ 
+      id: 12,
+      type: "Astrophotography",
+      src: 'img/gallery/12.jpg',
+      tum: 'img/gallery/12.jpg',
+      cp: "All good things are wild and free"
+    },
+    { 
+      id: 13,
+      type: "Nature",
+      src: 'img/gallery/13.jpg',
+      tum: 'img/gallery/13.jpg',
+      cp: ""
+    },
+    { 
+      id: 14,
+      type: "Astrophotography",
+      src: 'img/gallery/14.jpg',
+      tum: 'img/gallery/14.jpg',
+      cp: "Actually, it’s nature itself that creates the most beautiful pictures, I’m only choosing the perspective."
+    },
+    { 
+      id: 15,
+      type: "Astrophotography",
+      src: 'img/gallery/15.jpg',
+      tum: 'img/gallery/15.jpg',
+      cp: "Nature always wears the colors of the spirit."
+    },
+    
+    
+    
+];
---
+++ photography-website/js/gallary.js
@@ -0,0 +1,151 @@
+$(document).ready(function(){
+  var galleryOne = $("#galleryOne");
+  var seeMore1 = $("#seeMore1");
+  var data = ``;
+  var limit = 8;
+  var seeMoreCount = 8;
+  var cd = limit;
+  var numOfImg = 0;
+  var PotFilterOff = true;
+  var PText = '';
+  var itemsArray = [];
+
+
+  for (var i = 0; i < mainData.length; i++){
+    if(i === limit ) break;
+    data += `
+      <a data-cp="${mainData[i].cp}" 
+         href="${mainData[i].src}">
+        <img src="${mainData[i].tum}">
+      </a>
+    `;
+    numOfImg = i; 
+    var zx = itemsArray.findIndex(
+      function(item, i) {
+      return item == i;
+    });
+
+    if(zx == -1){ 
+      itemsArray.push(mainData[i].id);
+    }
+  }
+  galleryOne.html(data);
+  $('.gallery a').simpleLightbox({  captionAttribute:'data-cp'});
+
+// seemore button in website 
+  seeMore1.click(function(){
+    if(PotFilterOff){
+      cd += limit;// next set of images to be displayed.
+      if (cd - limit == limit) { // exceeds the limit.
+        var x = cd - limit; 
+      }else {var x = cd - limit +1; numOfImg += 1;}
+
+      for (var i2 = x; i2 < mainData.length; i2++) {// checking if its full to see if u we can display more images or not.
+          if(cd == numOfImg) break;
+          data += `
+            <a data-cp="${mainData[i2].cp}" 
+               href="${mainData[i2].src}">
+              <img src="${mainData[i2].tum}">
+            </a>
+          `;
+
+          var zx = itemsArray.findIndex(
+          function(item, i2) {
+            return item == i2;
+          });
+
+          if(zx == -1){
+            itemsArray.push(mainData[i2].id);
+          }
+          numOfImg += 1;
+      }
+    }else { //if potfilteroff is not true then 
+      cd+=limit;
+        var subMainData = mainData.filter( //we are filtering the main data for the particular data and storing it in submaindata. 
+        function(item) {
+            return item.type == PText;// select the photos based on filter.
+        });
+       
+        if(numOfImg == subMainData.length){  
+           seeMore1.text("That's it!");
+        }
+
+      for (var i5 = 0; i5 < mainData.length; i5++) {
+          if(cd == numOfImg) break;
+          var id = mainData[i5].id;
+          var zx = itemsArray.findIndex(
+          function(item) {
+            return item === id;
+          });
+          if (zx !== -1) continue;
+          if (PText != "All") if(mainData[i5].type != PText) continue;
+          itemsArray.push(id);
+          data += `
+            <a data-cp="${mainData[i5].cp}" 
+               href="${mainData[i5].src}">
+              <img src="${mainData[i5].tum}">
+            </a>
+          `;
+          numOfImg += 1;
+      }
+    }
+
+    if(mainData.length == numOfImg || mainData.length < numOfImg){
+       seeMore1.text("That's it!");
+    }
+
+    galleryOne.html(data);
+    $('.gallery a').simpleLightbox({  captionAttribute:'data-cp'});
+  });
+
+    //filter
+    var potFilter = $(".pot-filter ");
+    potFilter.click(function(e){ 
+      numOfImg = 0;
+      cd = limit;
+      itemsArray = []; 
+      seeMore1.text("See More");
+      PotFilterOff = false;
+      PText = $(this).html();
+
+      var parent = $(this).parent()[0];
+
+      for (var i3 = 0; i3 < $(parent).children().length; i3++) {
+        $($(parent).children()[i3]).removeClass('active-pf');
+      }
+      $(this).addClass("active-pf");
+      galleryOne.fadeTo(100, .1);//opaque is required to blur out the other images 
+
+      setTimeout(function(){
+        galleryOne.fadeTo(100, 1);
+        galleryOne.html("");
+        data = "";
+
+        for (var i4 = 0; i4 < mainData.length; i4++) {
+          if(numOfImg === limit ) break;
+          if(mainData[i4].type != PText && PText != "All") continue;
+          data += `
+            <a data-cp="${mainData[i4].cp}" 
+               href="${mainData[i4].src}">
+              <img src="${mainData[i4].tum}">
+            </a>
+          `;
+          numOfImg += 1;
+          var id = mainData[i4].id;
+          var zx = itemsArray.findIndex(
+          function(item) {
+            return item == id;
+          });
+
+          if(zx == -1){
+            itemsArray.push(id);
+          }
+        }
+        galleryOne.html(data);
+          $('.gallery a').simpleLightbox({  captionAttribute:'data-cp'});
+        
+
+      }, 500);
+
+    });
+});
